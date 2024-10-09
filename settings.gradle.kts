37eba1f5f964ed71cc544497801d8e253b4ee994
TheDarkDev <159413491+AlancitoPaladin@users.noreply.github.com>
2024-10-09 12:00:03 -0600

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io") // Añadimos JitPack para resolver dependencias adicionales
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // Añadimos JitPack aquí también
+    }
+}
+
+rootProject.name = "aartrad"
+include(":app")
