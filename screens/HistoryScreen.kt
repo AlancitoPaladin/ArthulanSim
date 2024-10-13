64280b191cac22654ce6966ccc52ba2269627b2d
TheDarkDev <159413491+AlancitoPaladin@users.noreply.github.com>
2024-10-13 15:03:24 -0600

    Update HistoryScreen.kt
    cambie la pantalla del usuario

--- screens/HistoryScreen.kt
+++ screens/HistoryScreen.kt
@@ -1,34 +1,248 @@
 package com.itsm.aartrad.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.itsm.aartrad.R
 import com.itsm.aartrad.components.Toolbar
 
+
 @Composable
-fun HistoryScreen(navController: NavController) {
+fun UserScreen(navController: NavController) {
     Scaffold(
         topBar = { Toolbar(navController = navController) }
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(paddingValues)
+                .background(Color(0xF804152E))
         ) {
-            Text(
-                text = "Hola, historial",
-                color = Color.White
-            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(topStart = 35.dp, topEnd = 35.dp))
+                    .background(Color(0xF804152E))
+                    .padding(20.dp)
+            ) {
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "Tarjetas",
+                            color = Color.White,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "Menu",
+                            modifier = Modifier
+                                .rotate(90f)
+                                .size(35.dp)
+                                .align(Alignment.End),
+                            tint = Color.White
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .padding(5.dp),
+                        shape = RoundedCornerShape(15.dp)
+                    ) {
+                        Text(
+                            text = "Tarjeta física",
+                            color = Color.Black,
+                            modifier = Modifier
+                                .padding(start = 15.dp, end = 15.dp, top = 5.dp, bottom = 5.dp),
+                            fontSize = 14.sp
+                        )
+                    }
+
+                    Card(
+                        modifier = Modifier.padding(5.dp),
+                        shape = RoundedCornerShape(15.dp)
+                    ) {
+                        Text(
+                            text = "Tarjeta virtual",
+                            color = Color.Gray,
+                            modifier = Modifier.padding(
+                                start = 15.dp,
+                                end = 15.dp,
+                                top = 5.dp,
+                                bottom = 5.dp
+                            ),
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                            .fillMaxWidth()
+                            .height(220.dp),
+                        shape = RoundedCornerShape(25.dp),
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color(0xF804152E)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.card),
+                                contentDescription = "Target",
+                                modifier = Modifier
+                                    .padding(20.dp)
+                            )
+                        }
+                    }
+                }
+
+
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Text(
+                    text = "Opciones",
+                    color = Color.White,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(20.dp))
+                                .background(Color.White)
+                                .size(65.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.send),
+                                contentDescription = "Send",
+                                modifier = Modifier.size(35.dp),
+                                colorFilter = ColorFilter.tint(Color.Black)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "Depositar",
+                            color = Color.White.copy(alpha = 0.5f),
+                            fontSize = 14.sp
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(20.dp))
+                                .background(Color.White)
+                                .size(65.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.retire),
+                                contentDescription = "Request",
+                                modifier = Modifier.size(35.dp),
+                                colorFilter = ColorFilter.tint(Color.Black)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "Retirar",
+                            color = Color.White.copy(alpha = 0.5f),
+                            fontSize = 14.sp
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(20.dp))
+                                .background(Color.White)
+                                .size(65.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.history),
+                                contentDescription = "History",
+                                modifier = Modifier.size(35.dp),
+                                colorFilter = ColorFilter.tint(Color.Black)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "Historial",
+                            color = Color.White.copy(alpha = 0.5f),
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
         }
     }
 }
